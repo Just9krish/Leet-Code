4bb42280318bb9b73ecf532c91f7283c8b205d6a
just9krish <just9krish434@gamil.com>
2022-07-16 17:07:51 +0530

    ok one more challenge of string completed with reduce method

---
+++ String/find longest word in string with using reduce.js
@@ -0,0 +1,12 @@
+function findLongestWordLength(str) {
+  return str.split(" ").reduce((longest, word) => {
+    return Math.max(longest, word.length);
+  }, 0);
+}
+
+console.log(
+  findLongestWordLength("The quick brown fox jumped over the lazy dog")
+);
+
+// reduce take four arguments
+reduce(acumulator, currentValue)
