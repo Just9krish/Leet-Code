0dd6082ac71d46934b4cc2a6157b53d9d6926faf
just9krish <just9krish434@gamil.com>
2022-07-16 16:24:18 +0530

    ok one more challenge of string completed with brute force method

--- String/find longest string in sentance.js
+++ String/find longest string in sentance.js
@@ -1,14 +1,20 @@
 function findLongestWordLength(str) {
-    let words = str.split(' ');
-    let maxLength = 0;
+  let words = str.split(" ");
+  let maxLength = 0;
+//   console.log(words)
+//   console.log(maxLength)
 
-    for(let i = 0; i < words.length; i++) {
-        if(words[i].length > maxLength) {
-            maxLength = words[i].length;
-        }
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= maxLength) {
+      maxLength = words[i].length;
+    //   console.log(words[i])
+//   console.log(maxLength)
     }
+  }
 
-    return maxLength;
+  return maxLength;
 }
 
-console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(
+  findLongestWordLength("The quick brown fox jumped over the lazy dog")
+);
