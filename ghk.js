8de47bf7fc61b49c34c1d84289849863436c9e46
just9krish <just9krish434@gamil.com>
2022-07-23 16:31:41 +0530

    practicing some js

---
+++ ghk.js
@@ -0,0 +1,39 @@
+// let prices = {
+//   banana: 1,
+//   orange: 2,
+//   meat: 4,
+// };
+
+
+// let doublePrice = Object.fromEntries(
+//     Object.entries(prices).map(entry => [entry[0], entry[1] * 2])
+// )
+
+// console.log(doublePrice.meat) 
+
+// let fruits = {
+//   banana: 1,
+//   orange: 2,
+//   meat: 4,
+// };
+
+// let b = {};
+
+// for (let fruit in fruits) {
+//     b[fruit] = fruits[fruit] * 2;
+// }
+
+// console.log(b.meat)
+
+// let arr = [ ['dfd', 'd'], ['dagd', 'dfd'], ['dege', 'df']]
+
+// let gh = Object.fromEntries(arr);
+
+// console.log(gh['dfd'])
+
+
+console.log("start");
+setTimeout(() => {
+    console.log("time out")
+});
+console.log('end')
